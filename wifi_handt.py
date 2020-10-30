14f6efd06eb416cd6376fbf4ae582106adcc329c
Dai Quoc Bui <quocdai.bui@dzsi.com>
2020-10-30 11:32:34 +0700

    Auto add wifi profile

--- wifi_handt.py
+++ wifi_handt.py
@@ -4,12 +4,11 @@ import paramiko
 import time
 import sys
 import csv
+import xml.etree.ElementTree as ET 
+from xml.etree.ElementTree import ElementTree
 from pandas import DataFrame
 
-ssh_1 = paramiko.SSHClient()
-ssh_1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_2 = paramiko.SSHClient()
-ssh_2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
 #==========#
 ssid_list ="SSID.txt"
 #==========#
@@ -33,11 +32,56 @@ with open(ssid_list, 'r') as f:
 	print("List of SSID: ",ssid_list)
 	number_of_ssid = len(ssid_list)
 
+
+
 def ssh_to_pc(ssh,Host,Port,User,Pass):
 	print("Connecting to {}...".format(Host))
 	ssh.connect(Host, Port, User, Pass, timeout = 2)
-	print("=== Connected success ===")
+	#print("=== Connected success ===")
 	stdin, stdout, stderr = ssh.exec_command("")
+
+def create_wifi_profile(wifi_profile, passwd):
+	config = """<?xml version=\"1.0\"?>
+	<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+		<name>"""+wifi_profile+"""</name>
+		<SSIDConfig>
+			<SSID>
+				<name>"""+wifi_profile+"""</name>
+			</SSID>
+		</SSIDConfig>
+		<connectionType>ESS</connectionType>
+		<connectionMode>auto</connectionMode>
+		<MSM>
+			<security>
+			<authEncryption>
+				<authentication>WPA2PSK</authentication>
+				<encryption>AES</encryption>
+				<useOneX>false</useOneX>
+			</authEncryption>
+			<sharedKey>
+				<keyType>passPhrase</keyType>
+				<protected>false</protected>
+				<keyMaterial>"""+passwd+"""</keyMaterial>
+			</sharedKey>
+			</security>
+		</MSM>
+	</WLANProfile>"""
+
+	f = open(wifi_profile + '.xml', 'w')
+	f.write(config)
+	f.close()
+
+
+
+# ftp_client = ssh_1.open_sftp()
+# ftp_client.put('daibq.xml', 'D:\\daibq.xml')
+# print("put file")
+# ftp_client.close()
+# stdin, stdout, stderr = ssh_1.exec_command("netsh wlan add profile filename=D:\\daibq.xml interface=Wi-Fi")
+# time.sleep(3)
+# stdin, stdout, stderr = ssh_1.exec_command("netsh wlan connect name=aaaaaaaaaaa_ah ssid=aaaaaaaaaaa_ah interface=Wi-Fi")
+# ssh_1.close()
+# exit()
 
 def connect_to_ssid(ssh,ssid):
 	subnet = "192.168.1."
@@ -93,6 +137,10 @@ def summary_result():
 	df.to_excel('summary_result.xlsx')
 
 if __name__ == '__main__':
+	ssh_1 = paramiko.SSHClient()
+	ssh_1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	ssh_2 = paramiko.SSHClient()
+	ssh_2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh_to_pc(ssh_1, Host_1, Port_1, User_1, Pass_1)
 	ssh_to_pc(ssh_2, Host_2, Port_2, User_2, Pass_2)
 	for ssid in ssid_list:
@@ -107,6 +155,8 @@ if __name__ == '__main__':
 				stdin, stdout, stderr = ssh_1.exec_command('netsh interface ip show addresses Wi-Fi | find "IP Address"')
 				sys.exit()
 			ping(ssh_1)
+	ssh_1.close()
+	ssh_2.close()
 	summary_result()
 		#ping_result.clear()
 #		print(ping_result)
