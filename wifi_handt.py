3ede2699e5dcf643295632c7cf1aa6f67d4c96f9
handt-dev <nguyendinhthanhha@gmail.com>
2020-11-13 15:42:42 +0700

    Auto get host information to ssh. Add function delete profile after finish

--- wifi_handt.py
+++ wifi_handt.py
@@ -9,28 +9,16 @@ import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import ElementTree
 from pandas import DataFrame
 
-
-# #==========#
-# ssid_list ="SSID.txt"
-#==========#
-Host_1 = "10.72.69.69"
-Port_1 = "22"
-User_1 = "handt"
-Pass_1 = "Dzs1234@"
-#==========#
-Host_2 = "10.72.71.71"
-Port_2 = "22"
-User_2 = "daibq"
-Pass_2 = "Dzs1234@"
-ping_result = []
-
-
-
 def ssh_to_pc(ssh,Host,Port,User,Pass):
 	print("Connecting to {}...".format(Host))
-	ssh.connect(Host, Port, User, Pass, timeout = 2)
-	#print("=== Connected success ===")
+	ssh.connect(Host, Port, User, Pass, timeout = 5)
+	print("=== Connected success ===")
 	stdin, stdout, stderr = ssh.exec_command("")
+
+def get_host_profile_from_json(json_file):
+	with open(json_file) as file:
+		data = json.load(file)
+	return data
 
 def create_wifi_profile(wifi_profile, passwd):
 	config = """<?xml version=\"1.0\"?>
@@ -72,21 +60,20 @@ def create_wifi_profile_from_json(json_file):
 		wifi_profile_list.append(wifi_profile)
 	return wifi_profile_list
 
-
-
 def put_file_via_ssh(ssh, file_name):
 	ftp_client = ssh.open_sftp()
 	remote_file = "D:\\{}".format(file_name)
 	ftp_client.put(file_name, remote_file)
 	ftp_client.close()
 
-
-def connect_to_ssid(ssh, wifi_profile):
-	subnet = "192.168.1."
-	print("\nAdd wifi profile {}".format(wifi_profile))
+def add_wifi_profile(ssh, wifi_profile, Host):
+	print("Add wifi profile {} for PC {}".format(wifi_profile, Host))
 	wifi_profile_path = "D:\\\\" + wifi_profile
-	stdin, stdout, stderr = ssh_1.exec_command("netsh wlan add profile filename={} interface=Wi-Fi".format(wifi_profile_path))
-	print("\nConnecting to SSID name {}...".format(wifi_profile))
+	stdin, stdout, stderr = ssh.exec_command("netsh wlan add profile filename={} interface=Wi-Fi".format(wifi_profile_path))
+
+def connect_to_ssid(ssh, wifi_profile, Host):
+	subnet = "192.168.1."
+	print("\nConnecting to SSID name {} of PC {}...".format(wifi_profile,Host))
 	stdin, stdout, stderr = ssh.exec_command("netsh wlan connect name={} ssid={}".format(wifi_profile, wifi_profile))
 	time.sleep(3)
 	status = stdout.readlines()
@@ -108,7 +95,7 @@ def connect_to_ssid(ssh, wifi_profile):
 	return ip_address
 
 def ping(ssh, ip_address):
-	print("Start to ping SSID...")
+	print("Start to ping SSID ...")
 	stdin, stdout, stderr = ssh.exec_command("ping {}".format(ip_address))
 	result = stdout.readlines()
 	result = "".join(result)
@@ -123,7 +110,6 @@ def ping(ssh, ip_address):
 	print(data)
 	ping_result.append(data)
 
-
 def summary_result(wifi_profile_list):
 	df = DataFrame(index=wifi_profile_list)
 	i = 0
@@ -134,30 +120,46 @@ def summary_result(wifi_profile_list):
 	print(df)
 	df.to_excel('summary_result.xlsx')
 
+def delete_wifi_profile(ssh, wifi_profile):
+	stdin, stdout, stderr = ssh.exec_command("del D:{}".format(wifi_profile))
+	stdin, stdout, stderr = ssh.exec_command("netsh wlan del profile name={} interface=Wi-Fi".format(wifi_profile))
+	check = stdout.readlines()
+	check = "".join(check)
+	check = check.find("deleted")
+	if check > -1 :
+		print("Delete profile succesful")
+	else :
+		print("Already deleted")
+
 if __name__ == '__main__':
+	ping_result = []
 	ssh_1 = paramiko.SSHClient()
 	ssh_1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh_2 = paramiko.SSHClient()
 	ssh_2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh_to_pc(ssh_1, Host_1, Port_1, User_1, Pass_1)
-	ssh_to_pc(ssh_2, Host_2, Port_2, User_2, Pass_2)
-
+	data = get_host_profile_from_json("host_data.json")
 	wifi_profile_list = create_wifi_profile_from_json("wifi_data.json")
-
+	ssh_to_pc(ssh_1,data[0]["Host"],data[0]["Port"],data[0]["User"],data[0]["Pass"])
+	ssh_to_pc(ssh_2,data[1]["Host"],data[1]["Port"],data[1]["User"],data[1]["Pass"])
+	print("------------------------")
 	for wifi_profile in wifi_profile_list:
 		put_file_via_ssh(ssh_1, wifi_profile)
 		put_file_via_ssh(ssh_2, wifi_profile)
-
+		add_wifi_profile(ssh_1, wifi_profile, data[0]["Host"])
+		add_wifi_profile(ssh_2, wifi_profile, data[1]["Host"])
+		print("------------------------")
 	for wifi_profile in wifi_profile_list:
-		ip_address = connect_to_ssid(ssh_2, wifi_profile)
+		ip_address = connect_to_ssid(ssh_2, wifi_profile, data[1]["Host"])
 		if ip_address == -1:
 			sys.exit()
-		
 		for wifi_profile in wifi_profile_list:
-			ret = connect_to_ssid(ssh_1, wifi_profile)
+			ret = connect_to_ssid(ssh_1, wifi_profile, data[0]["Host"])
 			if ret == -1:
 				sys.exit()
 			ping(ssh_1, ip_address)
+	for wifi_profile in wifi_profile_list:
+		delete_wifi_profile(ssh_1, wifi_profile)
+		delete_wifi_profile(ssh_2, wifi_profile)
 	ssh_1.close()
 	ssh_2.close()
 	summary_result(wifi_profile_list)
